8344e98c6ec422dcd053963612ce79307e5f4b89
Sharadkumar Zarapkar <56510676+sharad297@users.noreply.github.com>
2022-05-17 23:22:39 +0530

    Add files via upload

---
+++ Dijkstra.py
@@ -0,0 +1,33 @@
+graph={
+    'A':{'B':2,'C':3},
+    'B':{'D':3,'E':1},
+    'C':{'F':2},
+    'D':{},
+    'E':{'F':1},
+    'F':{}
+	}
+
+import heapq
+def dijkstra(graph,node):
+	distances={node:float('inf') for node in graph}
+	print("distances::",distances)
+	distances[node]=0
+	previous={node:None for node in graph}
+	queue=[(0,node)]
+	
+	while queue:
+		current_distance,current_node=heapq.heappop(queue)
+		# relaxation
+		for next_node,weight in graph[current_node].items():
+			distance_temp=current_distance+weight
+			if distance_temp<distances[next_node]:
+				distances[next_node]=distance_temp
+				previous[next_node]=current_node
+				heapq.heappush(queue,(distance_temp,next_node))
+			print("Distances::",distances)
+	return distances,previous
+
+#Driver Code
+Node_distance, Path = dijkstra(graph,'A')
+print(Node_distance)
+print(Path)
